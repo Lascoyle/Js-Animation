1d66c4af996bed35ba812d048887a7dbf9775079
Lascoyle <meursault45@live.com>
2021-06-27 14:40:10 +0200

    change assets backgrounds folder name

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -2,16 +2,16 @@
 
 // Background objetc settings
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = './assets/Background/sky.png'
+backgroundLayer1.src = './assets/backgrounds/sky.png'
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = './assets/Background/glacial_mountains.png'
+backgroundLayer2.src = './assets/backgrounds/glacial_mountains.png'
 const backgroundLayer3 = new Image();
-backgroundLayer3.src = './assets/Background/clouds_mg_1.png'
+backgroundLayer3.src = './assets/backgrounds/clouds_mg_1.png'
 const backgroundLayer4 = new Image();
-backgroundLayer4.src = './assets/Background/clouds_mg_2.png'
+backgroundLayer4.src = './assets/backgrounds/clouds_mg_2.png'
 const backgroundLayer5 = new Image();
-backgroundLayer5.src = './assets/Background/clouds_mg_3.png'
+backgroundLayer5.src = './assets/backgrounds/clouds_mg_3.png'
 const backgroundLayer6 = new Image();
-backgroundLayer6.src = './assets/Background/clouds_bg.png'
+backgroundLayer6.src = './assets/backgrounds/clouds_bg.png'
 
 export {backgroundLayer1, backgroundLayer2, backgroundLayer3, backgroundLayer4, backgroundLayer5, backgroundLayer6}
